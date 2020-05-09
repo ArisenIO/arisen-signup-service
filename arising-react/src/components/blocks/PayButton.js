ba667bbf96780a50126cc1f127aa474b79aa47b1
sbsashu <sbsashu143@gmail.com>
2020-05-09 14:48:56 +0530

    dwallet

--- arising-react/src/components/blocks/PayButton.js
+++ arising-react/src/components/blocks/PayButton.js
@@ -52,7 +52,7 @@ class PayButton extends Component {
                     //  window.location.href='https://github.com/arisenio/avote/releases'
                     Swal.fire({
                         title:'Download', // eslint-disable-next-line 
-                        html: 'You can use the dWallet  to interact with the Arisen network.'+' '+'<a href="https://github.com/arisenio/avote/releases" target="_blank">Click here to download.</a>',
+                        html: 'You can use the dWallet  to interact with the Arisen network.'+' '+'<a href="https://github.com/arisenio/dwallet/releases" target="_blank">Click here to download.</a>',
                     })
                 }
             })
